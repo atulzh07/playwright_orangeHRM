fb9a485425ae6c71ec1dc126394a20f071355b0f
atulzh07 <atulzh7@protonmail.com>
2024-11-30 21:24:15 +0545

    feat: added steps for login form flow

--- tests/orangeHRM.spec.js
+++ tests/orangeHRM.spec.js
@@ -1,9 +1,13 @@
 const { test } = require("@playwright/test");
+import { loginPage } from "./selectors";
 
 test("login to orangeHRM", async ({ browser, page }) => {
   const context = await browser.newContext();
   await page.goto(
     "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
   );
+  await page.locator(loginPage.userName).fill();
+  await page.locator(loginPage.password).fill();
+  await page.locator(loginPage.loginButton).click();
   await page.pause();
 });
