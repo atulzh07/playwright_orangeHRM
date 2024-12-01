ed2dee6277f65a46b4d3a5c5e0984c46fb552a4a
atulzh07 <atulzh7@protonmail.com>
2024-12-01 10:31:16 +0545

    git: .gitignore

--- tests/orangeHRM.spec.js
+++ tests/orangeHRM.spec.js
@@ -1,13 +1,17 @@
 const { test } = require("@playwright/test");
 import { loginPage } from "./selectors";
+import { credentials } from "../.auth/auth.setup";
 
 test("login to orangeHRM", async ({ browser, page }) => {
   const context = await browser.newContext();
   await page.goto(
     "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
   );
-  await page.locator(loginPage.userName).fill();
-  await page.locator(loginPage.password).fill();
+  await page.locator(loginPage.userName).fill(credentials.userName);
+  await page.locator(loginPage.password).fill(credentials.password);
   await page.locator(loginPage.loginButton).click();
+  await page.locator(
+    "oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"
+  );
   await page.pause();
 });
